e5b34f1a8c4e8509d48636909399a9c7aef8abed
Tania <tania.biryukova@gmail.com>
2017-10-20 23:12:19 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*var a = document.getElementsByClassName('scroll')[1];
+
+console.log(a);
+
+a.addEventListener("mouseover", function () {
+	a.style.color = "#FF9000";
+});
+
+a.addEventListener("mouseout", function () {
+	a.style.color = "#fff";
+});*/
